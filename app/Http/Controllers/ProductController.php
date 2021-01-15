1286262f4aa7f01cffc6a939a0284e7b32490d4c
Sameer FA <samervj@gmail.com>
2021-01-15 16:49:40 +0530

    product color fix test 2

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -45,8 +45,8 @@ class ProductController extends Controller
     public function getproduct($name, $color_name){
       $models = models::where('id',$name)->first();
       $colors = colors::where('model_id',$models->id)->get();
+      dd($name." ".$color_name." ".$models->id);
       $color  = colors::where('name', $color_name)->where('model_id',$models->id)->first();
-      dd($color);
       $pricing  = pricings::where('color_id', $color->id)->first();
       $orders = order_lists::where('color_id', $color->id)->where('prod_type','BASIC')->orWhere('prod_type','PREMIUM')->get();
       return view('product', compact('models','colors','color','pricing','orders'));
