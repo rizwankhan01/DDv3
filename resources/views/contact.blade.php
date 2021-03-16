2e5a0a16ab6123908d1edd1dfcd0ba8f62cad2a3
Sameer FA <samervj@gmail.com>
2021-03-16 13:06:01 +0530

    hidden field added

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -90,7 +90,7 @@
                                 <div class="col-lg-12 mt--30">
                                   <?php $one = rand(0,20); $two = rand(0,20); $ans = $one + $two; ?>
                                   <input type="number" placeholder="What is {{ $one }} + {{ $two }} ?" name="captcha">
-                                  <input type="number" name="ans" value="{{ $ans }}">
+                                  <input type="hidden" name="ans" value="{{ $ans }}">
                                 </div>
                                 <div class="col-lg-12 mt--30">
                                   <input type="submit" value="Submit" class="btn btn-primary">
