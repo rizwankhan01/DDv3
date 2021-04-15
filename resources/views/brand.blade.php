8395945ae7e8c9ecfeb7279fdfa236fe4a1a19e3
Sameer FA <samervj@gmail.com>
2021-04-16 00:20:28 +0530

    check file_exist url fix

--- resources/views/brand.blade.php
+++ resources/views/brand.blade.php
@@ -133,7 +133,7 @@
                                       <?php $image = $model->image; ?>
                                     @endif
                                   @endforeach
-                                  @if(!empty($image) AND file_exists(public_path().'/storage/{{ $image }}'))
+                                  @if(!empty($image) AND file_exists(public_path().'\storage\\'.$image))
                                     <img src="../storage/{{$image}}" class="logo-thumbnail" alt="{{ $image }} image">
                                   @else
                                     <img src="../storage/placeholder.png" class="logo-thumbnail" alt="{{ $model->image }} image">
