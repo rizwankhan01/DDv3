7a77a40baba0884657459afdcd95a139db4aa14d
Sameer FA <samervj@gmail.com>
2021-04-16 00:34:36 +0530

    test url 6

--- resources/views/brand.blade.php
+++ resources/views/brand.blade.php
@@ -133,10 +133,9 @@
                                       <?php $image = $model->image; ?>
                                     @endif
                                   @endforeach
-                                  @if(!empty($image) AND file_exists(public_path().'/storage/app/'.$image))
+                                  @if(!empty($image) AND file_exists('../storage/'.$image))
                                     <img src="../storage/{{$image}}" class="logo-thumbnail" alt="{{ $image }} image">
                                   @else
-                                    {{ public_path().'/storage/app/'.$image }}
                                     <img src="../storage/placeholder.png" class="logo-thumbnail" alt="{{ $model->image }} image">
                                   @endif
                                   <figcaption>{{ $model->brand->name }} {{$model->series }} {{ $model->name }}</figcaption>
