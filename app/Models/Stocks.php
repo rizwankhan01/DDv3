2778a59ef9506740060cb5eeada2420a17165a45
Sameer FA <samervj@gmail.com>
2021-07-27 13:30:59 +0530

    more typo fix

--- app/Models/Stocks.php
+++ app/Models/Stocks.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Stocks extends Model
+class stocks extends Model
 {
   protected $table = "stocks";
   protected $fillable = [
